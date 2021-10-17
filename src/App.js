a1399f4d458aa2f38020b64a3678d98a32aa93ce
José Martínez <38661492+josemfche@users.noreply.github.com>
2021-10-17 15:55:05 -0400

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
 
-  const wave = () => {
-    
-  }
-  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log('User is not connected to MetaMask');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+      }
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('We have found an account', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logOut = async () => {
+    setCurrentAccount('');
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        {/*         <button className="waveButton" onClick={wave}>
           Wave at Me
-        </button>
+        </button> */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        {currentAccount && (
+          <button className="waveButton" onClick={logOut}>
+            Log Out
+          </button>
+        )}
       </div>
     </div>
   );
